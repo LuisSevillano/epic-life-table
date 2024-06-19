1d853318868de6a067610839749d558405e1927b
LuisSevillano <luissevillanopires@gmail.com>
2024-06-19 23:08:59 +0200

    Initial commit

---
+++ src/lib/config.js
@@ -0,0 +1,6 @@
+export const siteTitle = 'Propuesta de compensación de inversiones en Epic Life';
+export const siteDescription = 'Formulario para calcular un posible equilibrio en las inversiones iniciales';
+export const siteURL = 'https://luissevillano.net/epic-form/index.html';
+export const home = 'https://luissevillano.net';
+
+export const siteImage = 'https://luissevillano.net/epic-form/cover.jpg';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+
+	import { siteTitle, siteDescription } from '$lib/config';
+</script>
+
+<main>
+	<header>
+		<h1>{siteTitle}</h1>
+		<h3>{siteDescription}</h3>
+	</header>
+
+</main>
+
+<style>
+	header {
+		padding: 3rem 0 1rem;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		position: relative;
+		margin-bottom: 3rem;
+	}
+	@media (min-width: 830px) {
+		header {
+			padding: 1rem 0;
+
+			width: auto;
+		}
+	}
+	h1 {
+		font-size: 2.5rem;
+	}
+	h3 {
+		font-size: 1.5rem;
+	}
+	h1,
+	h3 {
+		display: block;
+		width: 100%;
+		max-width: 50rem;
+		margin: 0 auto;
+	}
+	h3 {
+		color: black;
+		font-weight: lighter;
+		margin-top: 0.25rem;
+	}
+</style>
