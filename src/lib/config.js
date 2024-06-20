ba0d147d9a882f4296e7b0d31346aaa91cbc5720
LuisSevillano <luissevillanopires@gmail.com>
2024-06-20 15:33:20 +0200

    Actualiza meta

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,7 +4,7 @@ export const siteDescription =
 
 export const siteLongDesc =
 	'Esta página presenta una propuesta dirigida a equilibrar las contribuciones financieras de todos los miembros del proyecto Epic Life. Según está idea, a lo largo de los <b>años</b> todos los participantes contribuyen de manera equitativa al proyecto. Utilizando los sliders puedes ver cómo se ajustan las diferentes cuotas mensuales de <b>mantenimiento</b> y compensación.';
-export const siteURL = 'https://luissevillano.net/epic-form/index.html';
+export const siteURL = 'https://luissevillano.net/epic-life-table/index.html';
 export const home = 'https://luissevillano.net';
 
-export const siteImage = 'https://luissevillano.net/epic-form/cover.jpg';
+export const siteImage = 'https://luissevillano.net/epic-life-table/cover.jpg';
